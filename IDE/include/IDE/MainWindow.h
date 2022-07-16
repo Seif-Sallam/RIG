87f67d78a0df8fc4440a5416dbed542aa050d0dc
seif-sallam <seif_sallam@aucegypt.edu>
2022-07-16 22:07:31 +0200

    Setting up the dockspace

--- IDE/include/IDE/MainWindow.h
+++ IDE/include/IDE/MainWindow.h
@@ -20,6 +20,9 @@ namespace IDE
 		void Run();
 		~MainWindow();
 
+		inline static void SetWidth(int32_t value) { m_Width = value; }
+		inline static void SetHeight(int32_t value) { m_Height = value; }
+
 	private:
 		void HandleEvents();
 		void ImGuiLayer();
@@ -27,10 +30,31 @@ namespace IDE
 		void Render();
 
 		void InitilizeWindow();
+		void SetupLayout(ImGuiID &dockSpaceID);
+		void SetupDockspace();
+
+		ImVec2 DockspaceMenuBar();
+
+		void LogImGui();
+		void RegisterFileImGui();
+		void TextEditorImGui();
 
 	private:
+		static int32_t m_Width;
+		static int32_t m_Height;
 		GLFWwindow *m_Window;
 		TextEditor m_TextEditor;
+		ImGuiID m_DockID;
+		ImGuiID m_DockIDRight;
+		ImGuiID m_DockIDLeft;
+		ImGuiID m_DockIDBottom;
+		std::string m_TextEditorWindowName;
+		std::string m_RegisterFileWindowName;
+		std::string m_LogWindowName;
+
+		bool m_LayoutInitialized = false;
 	};
+
+
 
 }
--- IDE/src/IDE/MainWindow.cpp
+++ IDE/src/IDE/MainWindow.cpp
@@ -6,6 +6,7 @@
 
 #include <imguial_term.h>
 #include <imguial_button.h>
+#include <imgui_internal.h>
 
 #include <fstream>
 
@@ -13,24 +14,15 @@
 
 namespace IDE
 {
-	static void ErrorCallbackFunc(int error, const char *description)
-	{
-		fputs(description, stderr);
-	}
 
-	static void KeyCallbackFunc(GLFWwindow *window, int key, int scancode, int action, int mods)
-	{
-		// close window when ESC has been pressed
-		// if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		// glfwSetWindowShouldClose(window, GL_TRUE);
-	}
-
-	static void ViewPortResizeCallback(GLFWwindow *window, int width, int height)
-	{
-		glViewport(0, 0, width, height);
-	}
+	int32_t MainWindow::m_Width = 800;
+	int32_t MainWindow::m_Height = 600;
+	static void ErrorCallbackFunc(int error, const char *description);
+	static void KeyCallbackFunc(GLFWwindow *window, int key, int scancode, int action, int mods);
+	static void ViewPortResizeCallback(GLFWwindow *window, int width, int height);
 
 	MainWindow::MainWindow(uint32_t argc, const char *argv[])
+		: m_TextEditorWindowName("Text Editors"), m_LogWindowName("Log"), m_RegisterFileWindowName("Register File")
 	{
 		InitilizeWindow();
 		auto lang = TextEditor::LanguageDefinition::CPlusPlus();
@@ -55,6 +47,119 @@ namespace IDE
 		m_TextEditor.SetText(str);
 	}
 
+	void MainWindow::SetupLayout(ImGuiID &dockSpaceID)
+	{
+		m_LayoutInitialized = true;
+		m_DockID = dockSpaceID;
+
+		ImGui::DockBuilderRemoveNode(m_DockID);							   // Clear out existing layout
+		ImGui::DockBuilderAddNode(m_DockID, ImGuiDockNodeFlags_DockSpace); // Add empty node
+		ImGui::DockBuilderSetNodeSize(m_DockID, ImVec2(m_Width, m_Height));
+		m_DockIDLeft = ImGui::DockBuilderSplitNode(m_DockID, ImGuiDir_Left, 0.20f, NULL, &m_DockID);
+		m_DockIDRight = ImGui::DockBuilderSplitNode(m_DockID, ImGuiDir_Right, 0.20f, NULL, &m_DockID);
+		m_DockIDBottom = ImGui::DockBuilderSplitNode(m_DockID, ImGuiDir_Down, 0.20f, NULL, &m_DockID);
+		ImGui::DockBuilderDockWindow(m_TextEditorWindowName.c_str(), m_DockID);
+		ImGui::DockBuilderDockWindow(m_LogWindowName.c_str(), m_DockIDBottom);
+		ImGui::DockBuilderDockWindow(m_RegisterFileWindowName.c_str(), m_DockIDRight);
+		ImGui::DockBuilderFinish(m_DockID);
+	}
+
+	void MainWindow::SetupDockspace()
+	{
+		static const char *dockSpaceTitle = "Dockspace";
+		static auto dockSpaceID = ImGui::GetID(dockSpaceTitle);
+		if (!m_LayoutInitialized)
+			SetupLayout(dockSpaceID);
+
+		{
+			ImGuiWindowFlags dockSpaceWindowFlags = ImGuiWindowFlags_None | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |
+													ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_AlwaysAutoResize;
+			ImGui::SetNextWindowSize(ImVec2(m_Width, m_Height));
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+
+			ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+			ImGui::Begin(dockSpaceTitle, (bool *)nullptr, dockSpaceWindowFlags);
+			auto menuSize = DockspaceMenuBar();
+			ImVec2 windowSize(m_Width, m_Height);
+			auto size = ImVec2();
+			size.y = windowSize.y - menuSize.y - 2.0f;
+			ImGui::DockSpace(dockSpaceID, size, ImGuiDockNodeFlags_NoWindowMenuButton);
+			ImGui::End();
+			ImGui::PopStyleVar(3);
+		}
+	}
+
+	ImVec2 MainWindow::DockspaceMenuBar()
+	{
+		ImGui::BeginMenuBar();
+		{
+
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Reset Layout"))
+				{
+					m_LayoutInitialized = false;
+				}
+				if (ImGui::MenuItem("Save"))
+				{
+					auto textToSave = m_TextEditor.GetText();
+					/// save text....
+				}
+				if (ImGui::MenuItem("Quit", "Alt+F4"))
+				{
+					// We should close :D
+					glfwSetWindowShouldClose(m_Window, 1);
+				}
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Edit"))
+			{
+				bool ro = m_TextEditor.IsReadOnly();
+				if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+					m_TextEditor.SetReadOnly(ro);
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && m_TextEditor.CanUndo()))
+					m_TextEditor.Undo();
+				if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && m_TextEditor.CanRedo()))
+					m_TextEditor.Redo();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, m_TextEditor.HasSelection()))
+					m_TextEditor.Copy();
+				if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && m_TextEditor.HasSelection()))
+					m_TextEditor.Cut();
+				if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && m_TextEditor.HasSelection()))
+					m_TextEditor.Delete();
+				if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
+					m_TextEditor.Paste();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Select all", nullptr, nullptr))
+					m_TextEditor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(m_TextEditor.GetTotalLines(), 0));
+
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("View"))
+			{
+				if (ImGui::MenuItem("Dark palette"))
+					m_TextEditor.SetPalette(TextEditor::GetDarkPalette());
+				if (ImGui::MenuItem("Light palette"))
+					m_TextEditor.SetPalette(TextEditor::GetLightPalette());
+				if (ImGui::MenuItem("Retro blue palette"))
+					m_TextEditor.SetPalette(TextEditor::GetRetroBluePalette());
+				ImGui::EndMenu();
+			}
+		}
+		ImVec2 size = ImGui::GetItemRectSize();
+		ImGui::EndMenuBar();
+		return size;
+	}
 	void MainWindow::InitilizeWindow()
 	{
 		if (!glfwInit())
@@ -65,8 +170,8 @@ namespace IDE
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-		const int width = 1366;
-		const int height = 768;
+		const int width = m_Width;
+		const int height = m_Height;
 
 		m_Window = glfwCreateWindow(width, height, "RIG v0.0.2", NULL, NULL);
 
@@ -105,6 +210,8 @@ namespace IDE
 
 		ImGui_ImplGlfw_InitForOpenGL(m_Window, true);
 		ImGui_ImplOpenGL3_Init("#version 130");
+
+		ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 	}
 
 	MainWindow::~MainWindow()
@@ -127,6 +234,7 @@ namespace IDE
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
 			ImGui::NewFrame();
+			SetupDockspace();
 			ImGuiLayer();
 
 			Render();
@@ -140,78 +248,35 @@ namespace IDE
 
 	void MainWindow::ImGuiLayer()
 	{
-		ImGui::Begin("Test Window");
-		{
-			ImGui::Text("Hello, World! %d", 100);
+		RegisterFileImGui();
+		LogImGui();
+		TextEditorImGui();
+	}
 
-			if (ImGui::Button("Log"))
-			{
-			}
+	void MainWindow::LogImGui()
+	{
+		ImGui::Begin(m_LogWindowName.c_str());
+		{
+			ImGui::TextUnformatted("Log Window Text");
 		}
 		ImGui::End();
+	}
 
-		auto cpos = m_TextEditor.GetCursorPosition();
-		ImGui::Begin("Text Editor Demo", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
-		ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+	void MainWindow::RegisterFileImGui()
+	{
+		ImGui::Begin(m_RegisterFileWindowName.c_str());
 		{
-			if (ImGui::BeginMenuBar())
-			{
-				if (ImGui::BeginMenu("File"))
-				{
-					if (ImGui::MenuItem("Save"))
-					{
-						auto textToSave = m_TextEditor.GetText();
-						/// save text....
-					}
-					if (ImGui::MenuItem("Quit", "Alt-F4"))
-					{
-					}
-					ImGui::EndMenu();
-				}
-				if (ImGui::BeginMenu("Edit"))
-				{
-					bool ro = m_TextEditor.IsReadOnly();
-					if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
-						m_TextEditor.SetReadOnly(ro);
-					ImGui::Separator();
+			ImGui::TextUnformatted("Register File Window Text");
+		}
+		ImGui::End();
+	}
 
-					if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && m_TextEditor.CanUndo()))
-						m_TextEditor.Undo();
-					if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && m_TextEditor.CanRedo()))
-						m_TextEditor.Redo();
+	void MainWindow::TextEditorImGui()
+	{
+		auto cpos = m_TextEditor.GetCursorPosition();
 
-					ImGui::Separator();
-
-					if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, m_TextEditor.HasSelection()))
-						m_TextEditor.Copy();
-					if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && m_TextEditor.HasSelection()))
-						m_TextEditor.Cut();
-					if (ImGui::MenuItem("Delete", "Del", nullptr, !ro && m_TextEditor.HasSelection()))
-						m_TextEditor.Delete();
-					if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
-						m_TextEditor.Paste();
-
-					ImGui::Separator();
-
-					if (ImGui::MenuItem("Select all", nullptr, nullptr))
-						m_TextEditor.SetSelection(TextEditor::Coordinates(), TextEditor::Coordinates(m_TextEditor.GetTotalLines(), 0));
-
-					ImGui::EndMenu();
-				}
-
-				if (ImGui::BeginMenu("View"))
-				{
-					if (ImGui::MenuItem("Dark palette"))
-						m_TextEditor.SetPalette(TextEditor::GetDarkPalette());
-					if (ImGui::MenuItem("Light palette"))
-						m_TextEditor.SetPalette(TextEditor::GetLightPalette());
-					if (ImGui::MenuItem("Retro blue palette"))
-						m_TextEditor.SetPalette(TextEditor::GetRetroBluePalette());
-					ImGui::EndMenu();
-				}
-				ImGui::EndMenuBar();
-			}
-
+		ImGui::Begin(m_TextEditorWindowName.c_str(), nullptr, ImGuiWindowFlags_HorizontalScrollbar);
+		{
 			ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.mLine + 1, cpos.mColumn + 1, m_TextEditor.GetTotalLines(),
 						m_TextEditor.IsOverwrite() ? "Ovr" : "Ins",
 						m_TextEditor.CanUndo() ? "*" : " ",
@@ -234,5 +299,21 @@ namespace IDE
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		glfwSwapBuffers(m_Window);
+	}
+	void ViewPortResizeCallback(GLFWwindow *window, int width, int height)
+	{
+		MainWindow::SetWidth(width);
+		MainWindow::SetHeight(height);
+		glViewport(0, 0, width, height);
+	}
+	void KeyCallbackFunc(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		// close window when ESC has been pressed
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	void ErrorCallbackFunc(int error, const char *description)
+	{
+		fputs(description, stderr);
 	}
 }
